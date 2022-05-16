e35f77502661c15ec94a0ac1303a4095a96fadcf
Jon Laing <jon.dreaux.laing@gmail.com>
2022-05-16 14:03:22 -0400

    forgot comments on multidocs

--- dist/types/MultiDoc.d.ts
+++ dist/types/MultiDoc.d.ts
@@ -1,6 +1,6 @@
 import { Bulletin } from "./Bulletin";
 import { Dog } from "./Dog";
-import { EmbedField, Liked } from "./FirestoreBaseTypes";
+import { EmbedField, Liked, WithComments } from "./FirestoreBaseTypes";
 import { GUID } from "./GUID";
 import { Post } from "./Post";
 export interface LikedDogContent {
@@ -13,5 +13,5 @@ export interface LikedDogContent {
 export declare type MultiDoc<T extends LikedDogContent> = T & {
     dogs: Dog[];
 };
-export declare type MultiPost = MultiDoc<Liked<EmbedField<Post, "dog", Dog>>>;
-export declare type MultiBulletin = MultiDoc<Liked<EmbedField<Bulletin, "dog", Dog>>>;
+export declare type MultiPost = MultiDoc<WithComments<Liked<EmbedField<Post, "dog", Dog>>>>;
+export declare type MultiBulletin = MultiDoc<WithComments<Liked<EmbedField<Bulletin, "dog", Dog>>>>;
--- src/types/MultiDoc.ts
+++ src/types/MultiDoc.ts
@@ -1,6 +1,6 @@
 import { Bulletin } from "./Bulletin";
 import { Dog } from "./Dog";
-import { EmbedField, Liked } from "./FirestoreBaseTypes";
+import { EmbedField, Liked, WithComments } from "./FirestoreBaseTypes";
 import { GUID } from "./GUID";
 import { Post } from "./Post";
 
@@ -16,5 +16,9 @@ export type MultiDoc<T extends LikedDogContent> = T & {
   dogs: Dog[];
 };
 
-export type MultiPost = MultiDoc<Liked<EmbedField<Post, "dog", Dog>>>;
-export type MultiBulletin = MultiDoc<Liked<EmbedField<Bulletin, "dog", Dog>>>;
+export type MultiPost = MultiDoc<
+  WithComments<Liked<EmbedField<Post, "dog", Dog>>>
+>;
+export type MultiBulletin = MultiDoc<
+  WithComments<Liked<EmbedField<Bulletin, "dog", Dog>>>
+>;
